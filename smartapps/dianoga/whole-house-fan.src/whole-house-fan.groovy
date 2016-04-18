1cd6e7986415d09493c84faaad581e129af6aacf
Doug Cutting <cutting@gmail.com>
2016-04-18 09:57:58 -0700

    first version

--- smartapps/dianoga/whole-house-fan.src/whole-house-fan.groovy
+++ smartapps/dianoga/whole-house-fan.src/whole-house-fan.groovy
@@ -26,24 +26,14 @@ definition(
 
 preferences {
 	section("Outdoor") {
-		input "outTemp", "capability.temperatureMeasurement", title: "Outdoor Thermometer", required: true
+		input "outTemp", "capability.temperatureMeasurement", title: "Outdoor Thermometer"
 	}
     
     section("Indoor") {
-    	input "inTemp", "capability.temperatureMeasurement", title: "Indoor Thermometer", required: true
+    	input "inTemp", "capability.temperatureMeasurement", title: "Indoor Thermometer"
         input "minTemp", "number", title: "Minimum Indoor Temperature"
-        input "fans", "capability.switch", title: "Vent Fan", multiple: true, required: true
-    }
-    
-    section("Thermostat") {
-    	input "thermostat", "capability.thermostat", title: "Thermostat"
-    }
-    
-    section("Windows/Doors") {
-    	paragraph "[Optional] Only turn on the fan if at least one of these is open"
-        input "checkContacts", "enum", title: "Check windows/doors", options: ['Yes', 'No'], required: true 
-    	input "contacts", "capability.contactSensor", title: "Windows/Doors", multiple: true, required: false
-    }
+        input "fans", "capability.switch", title: "Vent Fan", multiple: true
+    }  
 }
 
 def installed() {
@@ -65,23 +55,15 @@ def initialize() {
     subscribe(outTemp, "temperature", "checkThings");
     subscribe(inTemp, "temperature", "checkThings");
     subscribe(thermostat, "thermostatMode", "checkThings");
-    subscribe(contacts, "contact", "checkThings");
 }
 
 def checkThings(evt) {
-	def outsideTemp = settings.outTemp.currentTemperature
-    def insideTemp = settings.inTemp.currentTemperature
-    def thermostatMode = settings.thermostat.currentThermostatMode
-    def somethingOpen = settings.checkContacts == 'No' || settings.contacts?.find { it.currentContact == 'open' }
+    def outsideTemp = settings.outTemp.currentValue('temperature')
+    def insideTemp = settings.inTemp.currentValue('temperature')
     
-    log.debug "Inside: $insideTemp, Outside: $outsideTemp, Thermostat: $thermostatMode, Something Open: $somethingOpen"
+    log.debug "Inside: $insideTemp, Outside: $outsideTemp, Thermostat: $thermostatMode"
     
     def shouldRun = true;
-    
-    if(thermostatMode != 'off') {
-    	log.debug "Not running due to thermostat mode"
-    	shouldRun = false;
-    }
     
     if(insideTemp < outsideTemp) {
     	log.debug "Not running due to insideTemp > outdoorTemp"
@@ -91,11 +73,6 @@ def checkThings(evt) {
     if(insideTemp < settings.minTemp) {
     	log.debug "Not running due to insideTemp < minTemp"
     	shouldRun = false;
-    }
-    
-    if(!somethingOpen) {
-    	log.debug "Not running due to nothing open"
-        shouldRun = false
     }
     
     if(shouldRun && !state.fanRunning) {
